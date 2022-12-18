ddc47a290afa8d75755555ff54cae7bd587587b8
Krymchuc <khrystyna.krymchuk@oa.edu.ua>
2022-12-19 01:49:46 +0200

    InitCommit

---
+++ Real_State_Catalog/Real_State_Catalog/Shared/Repository/IRepository/IRoomRepository.cs
@@ -0,0 +1,32 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_State_Catalog.Shared.Repository.IRepository
+{
+    public interface IRoomRepository
+    {
+
+        //Create a new hotel room DTO, then return that DTO
+    public Task<RoomDTO> CreateRoom(RoomDTO hotelRoomDTO);
+
+        //Update hotel
+        public Task<RoomDTO> UpdateRoom(int roomId,RoomDTO hotelRoomDTO);
+
+        //Get individiaul hotel
+        public Task<RoomDTO> GetRoom(int roomId, string checkIn = null, string checkOut = null);
+        public Task<int> DeleteRoom(int roomId);
+
+
+        //Get all hotels
+        public Task<IEnumerable<RoomDTO>> GetAllRoom(string checkIn = null, string checkOut = null);
+
+        //Get the name of the hotel room to validate booking
+
+        public Task<bool> IsBooked(int RoomId, string checkInDate, string checkOutDate);
+
+    }
+}
---
+++ Real_State_Catalog/Real_State_Catalog_Api/Controllers/AmenityController.cs
@@ -0,0 +1,36 @@
+﻿using Real_State_Catalog.Shared.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Real_State_Catalog_API.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    public class AmenityController : Controller
+    {
+        private readonly IAmenityRepository _hotelAmenityRepository;
+
+        //Access IHotelAmenityRepo so we can access the HotelAmenityRepo methods like GetAllHotelAmenity
+
+        public AmenityController( IAmenityRepository hotelAmenityRepository)
+        {
+        _hotelAmenityRepository = hotelAmenityRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAmenityList()
+        {
+            //Simple method of retrieving all amenities from db using the GetAllHotelAmenity method
+            var amenityList = await _hotelAmenityRepository.GetAllAmenity();
+            return Ok(amenityList);
+    
+        } 
+    }
+}
